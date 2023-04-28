0ac0fb8abb2ceddb55fb124d38294e70ab8ab2c9
Hashir <hashir.a@outlook.com>
2023-04-28 03:31:20 -0700

    added login page and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
+import Landing from './components/Landing';
+import Dashboard from './components/dashboard/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='login' Component={Signin} />
+          <Route path='register' Component={Signup} />
+          <Route path='landing' Component={Landing} />
+          <Route path='dashboard' Component={Dashboard} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
