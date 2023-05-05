83e15b4c3ca7b67005c6b591c11d77962839809b
Hashir <hashir.a@outlook.com>
2023-05-05 00:42:34 -0700

    added landing page component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/bg1.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
 
 const Landing = () => {
-  return <div>L</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+    </div>
+  );
 };
 
 export default Landing;
